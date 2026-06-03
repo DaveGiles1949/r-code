0d14f599dcf3a03d6384ac87386b22fa59433e94
DaveGiles1949 <63009168+DaveGiles1949@users.noreply.github.com>
2026-06-03 14:00:13 +1200

    Add files via upload

--- NZEP Benford'sLaw RCode/Todter M test decade sub-samples.R
+++ NZEP Benford'sLaw RCode/Todter M test decade sub-samples.R
@@ -3,6 +3,7 @@
 #############################################
 library(benford.analysis)
 library(stringr)
+library(readxl)
 
 coef_6070<- read_excel("C:/Users/OEM/Sync/Benford Law/NZEP/NZEP.xlsx",sheet="coef", range=cell_cols("A:M"))
 coef_6070<- coef_6070[!is.na(coef_6070)]
@@ -47,28 +48,6 @@ tstat_20<- tstat_20[!is.na(tstat_20)]
 
 # Main Benford analysis for 1st. and 2nd. digits
 # ----------------------------------------------
-
-coef_20<- c(coef_2020,coef_2021,coef_2022,coef_2023,coef_2024,coef_2025)
-coef_10<- c(coef_2010,coef_2011,coef_2012,coef_2013,coef_2014,coef_2015,coef_2016,coef_2017,coef_2018,coef_2019)
-coef_00<- c(coef_2000,coef_2001,coef_2002,coef_2003,coef_2004,coef_2005,coef_2006,coef_2007,coef_2008,coef_2009)
-coef_90<- c(coef_1990,coef_1991,coef_1992,coef_1993,coef_1994,coef_1995,coef_1996,coef_1997,coef_1998,coef_1999)
-coef_80<- c(coef_1980,coef_1981,coef_1983,coef_1985,coef_1986,coef_1987,coef_1988,coef_1989)
-coef_6070<- c(coef_1966,coef_1968,coef_1969,coef_1970,coef_1971,coef_1972,coef_1973,coef_1974,coef_1975,coef_1976, coef_1977,coef_1978,coef_1979)
-
-se_20<- c(se_2020,se_2021,se_2022,se_2023,se_2024,se_2025)
-se_10<- c(se_2010,se_2011,se_2012,se_2013,se_2014,se_2015,se_2016,se_2017,se_2018,se_2019)
-se_00<- c(se_2000,se_2001,se_2003,se_2004,se_2005,se_2006,se_2007,se_2008,se_2009)
-se_90<- c(se_1990,se_1992,se_1995,se_1996,se_1997,se_1998,se_1999)
-se_80<- c(se_1980,se_1981,se_1983,se_1988)
-se_6070<- c(se_1966,se_1968,se_1969,se_1971,se_1972,se_1973,se_1974,se_1975,se_1977,se_1979)
-
-tstat_20<- c(tstat_2021,tstat_2022,tstat_2024)
-tstat_10<- c(tstat_2010,tstat_2011,tstat_2012,tstat_2014,tstat_2015,tstat_2016,tstat_2017,tstat_2018,tstat_2019)
-tstat_00<- c(tstat_2000,tstat_2001,tstat_2002,tstat_2003,tstat_2004,tstat_2006,tstat_2008)
-tstat_90<- c(tstat_1990,tstat_1991,tstat_1992,tstat_1993,tstat_1994,tstat_1995,tstat_1996,tstat_1997,tstat_1998,tstat_1999)
-tstat_80<- c(tstat_1983,tstat_1985,tstat_1986,tstat_1987,tstat_1988,tstat_1989)
-tstat_6070<- c(tstat_1970,tstat_1971,tstat_1973,tstat_1974,tstat_1975,tstat_1976,tstat_1977,tstat_1978,tstat_1979)
-
 
 # 1966-1979
 #----------
